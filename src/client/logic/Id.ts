98b5c4d147102f725b8e98340cdf92c00fa4bbe9
Ryan Lynch <rlynch@dropbox.com>
2018-10-27 20:04:29 -0400

    deleted

--- src/client/logic/Id.ts
+++
@@ -1,9 +0,0 @@
-import { LogicLayer } from "../core";
-import { TodoScope, todos } from "../types";
-
-export class IdLayer extends LogicLayer<TodoScope> {
-  @todos.on.addTodo.update.nextId()
-  updateOnAddTodo(nextId: number) {
-    return nextId + 1;
-  }
-}
