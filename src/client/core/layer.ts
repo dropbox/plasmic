347d5f5de2c5fbaeffdd3e0b622c32c219208f7e
Ryan Lynch <rlynch@dropbox.com>
2018-10-28 21:59:46 -0400

    reorg, rename

--- src/client/core/layer.ts
+++
@@ -1,6 +0,0 @@
-import { Scope, Status, Triggers } from "./types";
-
-export class Layer<S extends Scope> {
-  readonly status: Status<S>;
-  readonly triggers: Triggers<S>;
-}
--- src/client/logic/id.ts
+++
@@ -1,9 +0,0 @@
-import { LogicLayer } from "../core/logic_layer";
-import { TodoScope, todos } from "../types";
-
-export class IdLayer extends LogicLayer<TodoScope> {
-  @todos.on.addTodo.update.nextId()
-  updateOnAddTodo(nextId: number) {
-    return nextId + 1;
-  }
-}
